524a9b3f4625ce50a293cc00ac57059cba4d8ee7
crowerks ws2 <crowerks-ws2@crowerkss-iMac-2.local>
2017-07-11 16:58:31 -0700

    removed example component

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -15,7 +15,6 @@ window.Vue = require('vue');
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-Vue.component('example', require('./components/Example.vue'));
 Vue.component('admin-widget', require('./components/admin-widget.vue'));
 const app = new Vue({
     el: '#app'
