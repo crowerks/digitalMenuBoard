7d4a65093c08c38a4ae673ccad7de4fdf2c4fe0d
crowerks ws2 <crowerks-ws2@crowerkss-iMac-2.local>
2017-07-10 18:51:13 -0700

    created component

---
+++ resources/assets/js/components/admin-widget.vue
@@ -0,0 +1,18 @@
+
+<template>
+   <div class="admin-widget">
+          <div class="btn-group-vertical" role="group" aria-label="...">
+              <a v-bind:href="admin/register"> <button type="button" class="btn btn-default">Register</button></a>
+              <a v-bind:href="admin/show"> <button type="button" class="btn btn-default">Users</button></a>
+  </div>
+  </div>
+
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
