2896073bd4e0a4de37a9343da846379b860c6323
crowerks ws2 <crowerks-ws2@crowerkss-iMac-2.local>
2017-07-10 18:52:06 -0700

    created view

--- resources/views/admin/admin.blade.php
+++ resources/views/admin/admin.blade.php
@@ -1,9 +1,22 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="admin-widget">
-        <h1>this is admin</h1>
-
-    </div>
+    {{--<div class="admin-widget">--}}
+        {{--<div class="btn-group-vertical" role="group" aria-label="...">--}}
+            {{--<a href="{{ url('admin/register') }}"> <button type="button" class="btn btn-default">Register</button></a>--}}
+            {{--<a href="{{ url('admin/show') }}"> <button type="button" class="btn btn-default">Users</button></a>--}}
+        {{--</div>--}}
+        <admin-widget></admin-widget>
 
 @endsection
+
+    {{--@if (Route::has('login'))--}}
+    {{--<div class="top-right links">--}}
+    {{--@if (Auth::check() && Auth::user()->isAdmin())--}}
+
+    {{--@elseif (Auth::guest())--}}
+
+
+    {{--@endif--}}
+    {{--</div>--}}
+    {{--@endif--}}
