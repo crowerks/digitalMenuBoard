77e0f5816ade06a11b0f33b1b95db754ec904e49
crowerks ws2 <crowerks-ws2@crowerkss-iMac-2.local>
2017-07-11 16:59:09 -0700

    fixed href url's

--- resources/assets/js/components/admin-widget.vue
+++ resources/assets/js/components/admin-widget.vue
@@ -1,11 +1,12 @@
 
 <template>
-   <div class="admin-widget">
+    <div class="admin-widget">
           <div class="btn-group-vertical" role="group" aria-label="...">
-              <a v-bind:href="admin/register"> <button type="button" class="btn btn-default">Register</button></a>
-              <a v-bind:href="admin/show"> <button type="button" class="btn btn-default">Users</button></a>
-  </div>
-  </div>
+              <a v-bind:href="url('admin')"> <button type="button" class="btn btn-default">Dashboard</button></a>
+              <a v-bind:href="url('admin/register')"> <button type="button" class="btn btn-default">Register</button></a>
+              <a v-bind:href="url('admin/show')"> <button type="button" class="btn btn-default">Users</button></a>
+          </div>
+      </div>
 
 </template>
 
