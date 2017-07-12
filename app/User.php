cbaf163668c1feff62f5c818a2bbfe563ff27585
crowerks ws2 <crowerks-ws2@crowerkss-iMac-2.local>
2017-07-11 18:09:22 -0700

    formatted

--- app/User.php
+++ app/User.php
@@ -34,7 +34,7 @@ class User extends Authenticatable
 
     public function isAdmin()
     {
-        $role = 'administrator';
+        $role = 'Administrator';
         return $this->role->name == $role;
     }
 }
