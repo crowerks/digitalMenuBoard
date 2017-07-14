82c81426402995b13cb1dc6aa3ed7d99f0363f37
crowerks ws2 <crowerks-ws2@crowerkss-iMac-2.local>
2017-07-13 18:05:23 -0700

    added links

--- resources/assets/js/components/subscriber-widget.vue
+++ resources/assets/js/components/subscriber-widget.vue
@@ -1,11 +1,15 @@
 
 <template>
-     <div class="subscriber-widget">
-            <div class="btn-group-vertical" role="group" aria-label="...">
-                <a href="http://localhost/digitalMenuBoard/public/subscriber"> <button type="button" class="btn btn-default">Dashboard</button></a>
-            </div>
-        </div>
-
+   <div class="subscriber-widget">
+       <div class="btn-group-vertical" role="group" aria-label="...">
+           <a href="http://localhost/digitalMenuBoard/public/subscriber"> <button type="button" class="btn btn-default">Dashboard</button></a>
+           <a href="http://localhost/digitalMenuBoard/public/subscriber/products"> <button type="button" class="btn btn-default">Products</button></a>
+           <a href="http://localhost/digitalMenuBoard/public/subscriber/trash"> <button type="button" class="btn btn-default">Trash Bin</button></a>
+           <a href="http://localhost/digitalMenuBoard/public/subscriber/headings"> <button type="button" class="btn btn-default">Headings</button></a>
+           <a href="http://localhost/digitalMenuBoard/public/subscriber/pricing"> <button type="button" class="btn btn-default">Pricing</button></a>
+            <a href="http://localhost/digitalMenuBoard/public/subscriber/specials"> <button type="button" class="btn btn-default">Specials</button></a>
+       </div>
+  </div>
 </template>
 
 <script>
