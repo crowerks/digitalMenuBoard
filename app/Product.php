55bccfe592ed496db666e08accbb6443c14752a4
crowerks ws2 <crowerks-ws2@crowerkss-iMac-2.local>
2017-07-13 18:00:45 -0700

    made properties available

--- app/Product.php
+++ app/Product.php
@@ -3,8 +3,16 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Product extends Model
 {
-    //
+    use SoftDeletes;
+    protected $dates = ['deleted_at'];
+
+    protected $fillable = [
+        'product_heading', 'flavor', 'order', 'deleted_at'
+
+    ];
+
 }
