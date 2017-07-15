b1e42952533f7da38633f74e83241a8ec52d5a54
crowerks ws2 <crowerks-ws2@crowerkss-iMac-2.local>
2017-07-14 18:58:02 -0700

    made active column fillable

--- app/Special.php
+++ app/Special.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Special extends Model
 {
-    protected $fillable = ['special'];
+    protected $fillable = ['special', 'active'];
+
+    public function scopeActive($query)
+    {
+        if (Special::where('active', 1)->first()) {
+            return Special::where('active', 1)->first()->special;
+        }
+        else {
+           return "Please input value";
+        }
+
+    }
+
 }
