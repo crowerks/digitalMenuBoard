ac2478611a952bf2452c6e4932e5162846cdece9
crowerks ws2 <crowerks-ws2@crowerkss-iMac-2.local>
2017-07-18 18:21:06 -0700

    commented out function call

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,6 +12,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         factory(App\Product::class, 20)->create();
-        factory(App\Heading::class, 5)->create();
+//        factory(App\Heading::class, 5)->create();
     }
 }
